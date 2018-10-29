fbc47c43f259c6f8036398418a5c0d47fa2063d8
nicolas <nicolas@mywebshop.org>
2018-10-29 13:56:11 +0100

    creation corebundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new OC\PlateformBundle\OCPlatformBundle(),
+            new NG\CoreBundle\NgCoreBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
